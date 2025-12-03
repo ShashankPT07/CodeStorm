50b51a5ba294de0e697eda846ed9f0f4fd5040a2
ShashankPT07 <shashankpt.xworks@gamil.com>
2025-12-03 11:10:40 +0530

    casting

---
+++ casting/src/com/xworks/casting/inernal/Animal.java
@@ -0,0 +1,12 @@
+package com.xworks.casting.inernal;
+
+public class Animal {
+    public static void main(String[] args) {
+        int price=100;
+        System.out.println("this is casting first method"+price);
+        double amount=price;
+        System.out.println("this is methos casting"+amount);
+        int sanjay=(int)amount;
+        System.out.println("this is casting call"+sanjay);
+    }
+}
